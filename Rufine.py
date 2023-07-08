267cfc726429d80b11bb4e73c0e9cb2d8d8fdf95
rufine45 <134793041+rufine45@users.noreply.github.com>
2023-07-08 08:38:25 +0545

    Update Rufine.py

--- Rufine.py
+++ Rufine.py
@@ -3,6 +3,6 @@ try:import httpx
 except:os.system('pip install httpx > /dev/null')
 os.system('git pull')
 try:
-    __import__("acc").menu()
+    __import__("rufine").menu()
 except Exception as e:
     exit(str(e))
